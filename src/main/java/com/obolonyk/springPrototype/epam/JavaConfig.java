d4f8c2ee67024052c4ca8eee47319fcf3fbe517f
U0L9511 <anton.obolonyk@esternibisp.com>
2022-05-01 17:22:01 +0300

    1. Create new implementation of the Policeman Benefits: a) new we have centralised place for creating new objects (if we want to use new implementation of the Policeman we go to the ObjectFactory and put to the map the implementation that you want) b) before the Factory gives the object it can make some adjustments So how we can make this adjustments?

--- src/main/java/com/obolonyk/springPrototype/epam/JavaConfig.java
+++ src/main/java/com/obolonyk/springPrototype/epam/JavaConfig.java
@@ -2,22 +2,31 @@ package com.obolonyk.springPrototype.epam;
 
 import org.reflections.Reflections;
 
+import java.util.Map;
 import java.util.Set;
 
 public class JavaConfig implements Config {
     private Reflections scanner;
+    private Map<Class, Class> ifcToImpl;
 
-    public JavaConfig(String packageToScan) {
+    public JavaConfig(String packageToScan, Map<Class, Class> ifcToImpl) {
         this.scanner = new Reflections(packageToScan);
+        this.ifcToImpl = ifcToImpl;
     }
 
     @Override
     public <T> Class<? extends T> getImplClass(Class<T> ifc) {
-        Set<Class<? extends T>> classes = scanner.getSubTypesOf(ifc);
-        //if the interface has more than one or any implementations we cannot go further
-        if (classes.size() != 1) {
-            throw new RuntimeException(ifc + " has 0 or more than 1 implementations");
-        }
-        return classes.iterator().next();
+        //if key is exist method return value, if not: it start the lambda, which returns the value
+        //which is gonna be saved in map
+        //we need this map to remember all the implementations of the interface
+        //and avoid scanning the package
+        return ifcToImpl.computeIfAbsent(ifc, aClass -> {
+            Set<Class<? extends T>> classes = scanner.getSubTypesOf(ifc);
+            //if the interface has more than one or any implementations we cannot go further
+            if (classes.size() != 1) {
+                throw new RuntimeException(ifc + " has 0 or more than 1 implementations please update your config");
+            }
+            return classes.iterator().next();
+        });
     }
 }
--- src/main/java/com/obolonyk/springPrototype/epam/ObjectFactory.java
+++ src/main/java/com/obolonyk/springPrototype/epam/ObjectFactory.java
@@ -3,18 +3,23 @@ package com.obolonyk.springPrototype.epam;
 
 import lombok.SneakyThrows;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*Singleton - the core of our application, which will be
 responsible for service's objects creation
 */
 public class ObjectFactory {
     private static ObjectFactory ourInstance = new ObjectFactory();
-    private Config config = new JavaConfig("com.obolonyk.springPrototype.epam");
+    private Config config;
 
     public static ObjectFactory getInstance() {
         return ourInstance;
     }
 
     private ObjectFactory() {
+       config = new JavaConfig("com.obolonyk.springPrototype.epam",
+                new HashMap<>(Map.of(Policeman.class, AngryPoliceman.class)));
     }
 
     @SneakyThrows
