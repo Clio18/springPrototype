9bb4d33894b0d5d2409996fc3b4b7b26145a4ec7
U0L9511 <anton.obolonyk@esternibisp.com>
2022-05-01 20:06:35 +0300

    1. PostConstruct annotation was added, which allows us to make adjustments after object configuration.

--- src/main/java/com/obolonyk/springPrototype/epam/PolicemanImpl.java
+++ src/main/java/com/obolonyk/springPrototype/epam/PolicemanImpl.java
@@ -1,8 +1,16 @@
 package com.obolonyk.springPrototype.epam;
 
+import javax.annotation.PostConstruct;
+
 public class PolicemanImpl implements Policeman {
     @InjectByType
     private Recommendator recommendator;
+
+    //this method is calling when the object is already adjusted
+    @PostConstruct
+    public void init() {
+        System.out.println(recommendator.getClass());
+    }
 
     @Override
     public void makePeopleLeaveRoom() {
